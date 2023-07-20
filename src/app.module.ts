2c0e1a80859fd565e3fc718d227e045210b23c1a
Evans Gacheru <evans.gacheru@andela.com>
2023-07-20 22:01:26 +0300

    create user auth and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { OrdersModule } from './orders/orders.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, TransactionsModule, OrdersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    TransactionsModule,
+    OrdersModule,
+    AuthModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  getLanding(): string {
     return 'Escrow P2P Platform';
   }
 }
