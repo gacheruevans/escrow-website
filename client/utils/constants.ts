d72d13fc505ec4e5f37e853f33eb5ae0d561fddb
Evans Gacheru <evans.gacheru@andela.com>
2023-09-26 23:00:26 +0300

    add escrow address to client side

--- client/utils/constants.ts
+++ client/utils/constants.ts
@@ -2,3 +2,4 @@ import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
 export const contractAddress = '0x82e238601134719f0D12ecfD660e4619994f5918';
+export const escrowAddress = '0x92B461cB15D8Ea0b89dBF404214BacB6B3Fc84Dd';
