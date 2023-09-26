a271fa321599fa7305da853c2190f465e0722fc2
Evans Gacheru <evans.gacheru@andela.com>
2023-09-26 22:41:34 +0300

    refactor code

--- smart-contract/contracts/Escrow.sol
+++ smart-contract/contracts/Escrow.sol
@@ -4,25 +4,25 @@ pragma solidity ^0.8.19;
 
 contract Escrow {
     address agent;
-    mapping(address => unit256) public deposits;
+    mapping(address => uint256) public deposits;
 
     modifier onlyAgent() {
         require(msg.sender == agent);
         _;
     }
 
-    constructor() public {
+    constructor() {
         agent = msg.sender;
     }
 
     function deposit(address payee) public payable onlyAgent {
         uint256 amount = msg.value;
-        deposits[payee] = deposit[payee] + amount;
+        deposits[payee] = deposits[payee] + amount;
     }
 
     function withdraw(address payable payee) public onlyAgent {
-        unit256 payment = deposit[payee];
-        deposite[payee] = 0;
+        uint256 payment = deposits[payee];
+        deposits[payee] = 0;
         payee.transfer(payment);
     }
 }
