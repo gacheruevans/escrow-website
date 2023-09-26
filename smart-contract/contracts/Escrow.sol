603b92c1eff40d915826b6caf85bf5f94d05295b
Evans Gacheru <evans.gacheru@andela.com>
2023-09-26 21:50:32 +0300

    add middleman agent

--- smart-contract/contracts/Escrow.sol
+++ smart-contract/contracts/Escrow.sol
@@ -1,3 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.19;
+
+contract Escrow {
+    address agent;
+    mapping(address => unit256) public deposits;
+
+    modifier onlyAgent() {
+        require(msg.sender == agent);
+        _;
+    }
+
+    constructor() public {
+        agent = msg.sender;
+    }
+
+    function deposit(address payee) public payable onlyAgent {
+        uint256 amount = msg.value;
+        deposits[payee] = deposit[payee] + amount;
+    }
+
+    function withdraw(address payable payee) public onlyAgent {
+        unit256 payment = deposit[payee];
+        deposite[payee] = 0;
+        payee.transfer(payment);
+    }
+}
